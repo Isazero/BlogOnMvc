80d57c31e5da438a88bf3d62c309e6a31b85ecf8
Isazero <36978572+Isazero@users.noreply.github.com>
2018-11-28 21:50:54 +0600

    Add files via upload

---
+++ Filters/ReqireRequestValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlog.Filters
+{
+    public class RequireRequestValue : ActionMethodSelectorAttribute
+    {
+        public RequireRequestValue(string valueName)
+        {
+            ValueName = valueName;
+        }
+
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return (controllerContext.HttpContext.Request[ValueName] != null);
+        }
+
+        public string ValueName { get; }
+    }
+
+}
